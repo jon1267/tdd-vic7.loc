dd930896cc1415326ff80420111825ce9cc11c5a
jon1267 <jonleev@gmail.com>
2020-08-27 14:43:42 +0300

    checkout && checkin feature tests

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,13 @@ Route::patch('/books/{book}', 'BooksController@update');
 Route::delete('/books/{book}', 'BooksController@destroy');
 
 Route::post('/author', 'AuthorsController@store');
+
+//BookCheckoutTest (Feature)
+Route::post('/checkout/{book}', 'CheckoutBookController@store');
+Route::post('/checkin/{book}', 'CheckinBookController@store');
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
