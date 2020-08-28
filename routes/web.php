d83aab6715a5905d6c01daab9436ae5852431bc3
jon1267 <jonleev@gmail.com>
2020-08-28 12:47:59 +0300

    last part, all for backend

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::post('/books', 'BooksController@store');
 Route::patch('/books/{book}', 'BooksController@update');
 Route::delete('/books/{book}', 'BooksController@destroy');
 
-Route::post('/author', 'AuthorsController@store');
+Route::post('/authors', 'AuthorsController@store');
 
 //BookCheckoutTest (Feature)
 Route::post('/checkout/{book}', 'CheckoutBookController@store');
--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -16,16 +16,35 @@ class AuthorManagementTest extends TestCase
     {
         //$this->withoutExceptionHandling();
 
-        $this->post('/author', [
-            'name' => 'Author name',
-            'dob'  => '1988-05-14'
-        ]);
+        $this->post('/authors', $this->data());
 
         $author = Author::all();
 
         $this->assertCount(1, $author);
         $this->assertInstanceOf(Carbon::class, $author->first()->dob);
         $this->assertEquals('14.05.1988', $author->first()->dob->format('d.m.Y'));
-        //$this->assertEquals('Author name', $author->first()->name);
+    }
+
+    /** @test */
+    public function a_name_is_required()
+    {
+        $response = $this->post('/authors', array_merge($this->data(), ['name' => '']));
+
+        $response->assertSessionHasErrors('name');
+    }
+
+    /** @test */
+    public function a_dob_is_required()
+    {
+        $response = $this->post('/authors', array_merge($this->data(), ['dob' => '']));
+
+        $response->assertSessionHasErrors('dob');
+    }
+
+    private function data() {
+        return [
+            'name' => 'Author name',
+            'dob'  => '1988-05-14'
+        ];
     }
 }
